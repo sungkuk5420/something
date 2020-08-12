790ad99c35f37ada4f53d5ec4a3492a4fa01d357
sungkuk5420 <sungkuk5420@gmail.com>
2020-08-12 22:36:21 +0900

    로그인화면 퍼블리싱

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,45 +1,28 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header bordered class="bg-primary text-white" height-hint="98">
-      <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="left = !left"/>
-
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
-          </q-avatar>Title
-        </q-toolbar-title>
-
-        <q-btn dense flat round icon="menu" @click="right = !right"/>
-      </q-toolbar>
-
-      <q-tabs align="left">
-        <q-route-tab to="/page1" label="Page One"/>
-        <q-route-tab to="/page2" label="Page Two"/>
-        <q-route-tab to="/page3" label="Page Three"/>
-      </q-tabs>
+    <q-header height-hint="98">
+      <img src="statics/icons/logo_something_main.png" alt />
     </q-header>
 
-    <q-drawer show-if-above v-model="left" side="left" behavior="desktop" bordered>
-      <!-- drawer content -->
-    </q-drawer>
-
-    <q-drawer show-if-above v-model="right" side="right" behavior="desktop" elevated>
-      <!-- drawer content -->
-    </q-drawer>
-
     <q-page-container>
-      <router-view/>
+      <router-view />
     </q-page-container>
 
-    <q-footer reveal class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
-          </q-avatar>Title
-        </q-toolbar-title>
-      </q-toolbar>
+    <q-footer reveal class="text-white">
+      <q-tabs align="left">
+        <q-route-tab to="/page1">
+          <img src="statics/icons/btn_inu_off.png" class="off active" alt />
+          <img src="statics/icons/btn_inu_on.png" class="on" alt />
+        </q-route-tab>
+        <q-route-tab to="/page2">
+          <img src="statics/icons/btn_help_off.png" class="off active" alt />
+          <img src="statics/icons/btn_help_on.png" class="on" alt />
+        </q-route-tab>
+        <q-route-tab to="/page3">
+          <img src="statics/icons/btn_log_off.png" class="off" alt />
+          <img src="statics/icons/btn_log_on.png" class="on active" alt />
+        </q-route-tab>
+      </q-tabs>
     </q-footer>
   </q-layout>
 </template>
@@ -47,10 +30,85 @@
 <script>
 export default {
   data() {
-    return {
-      left: false,
-      right: false
-    };
-  }
+    return {};
+  },
 };
 </script>
+
+<style lang="scss" >
+.q-header {
+  display: flex;
+  background-color: #f7d1c5;
+  justify-content: center;
+  img {
+    width: 150px;
+    margin-top: 8px;
+  }
+}
+.q-page {
+  background-color: #eff0f0;
+
+  & > .main {
+    flex: 1;
+    overflow: auto;
+    background-color: #eff0f0;
+    padding: 0 30px;
+
+    .inputbox {
+      background-color: #ffffff;
+      border: 0.5px solid #817e7f;
+      height: 50px;
+      padding: 0 10px;
+      border-top-width: 0;
+    }
+    .inputbox:first-child {
+      border-top-width: 0.5px;
+    }
+    .inputbox::placeholder {
+      color: #000000;
+    }
+    .checkbox-row {
+      display: flex;
+      align-items: center;
+      margin-top: 15px;
+    }
+    .checkbox {
+      border-radius: 50%;
+      color: #000000;
+      width: 20px;
+      height: 20px;
+      margin-right: 10px;
+    }
+    .btn1 {
+      margin-top: 15px;
+      background-color: #f7d1c5;
+      border: 0.5px solid #817e7f;
+      font-size: 15px;
+      height: 40px;
+    }
+    .btn2 {
+      margin-top: 10px;
+      border: 0;
+      font-size: 15px;
+      height: 40px;
+    }
+  }
+}
+
+.q-footer {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 10px 0;
+  background: white;
+}
+.q-footer img {
+  width: 60px;
+  cursor: pointer;
+  display: none;
+}
+
+.q-footer img.active {
+  display: block;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,31 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page class="login-page">
+    <div class="main">
+      <input type="text" class="inputbox" placeholder="학번" />
+      <input type="text" class="inputbox" placeholder="비밀번호" />
+      <label class="checkbox-row">
+        <input type="checkbox" class="checkbox" value="autologin" /> 자동로그인
+      </label>
+      <input type="button" class="btn1" value="로그인" />
+      <input type="button" class="btn1" value="회원가입" />
+      <input type="button" class="btn2" value="비밀번호 찾기" />
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: "PageIndex"
+  name: "PageIndex",
 };
 </script>
+
+<style lang="scss">
+.login-page {
+  .main {
+    padding-top: 100px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+}
+</style>
