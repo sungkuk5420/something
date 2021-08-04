118e334b18c317aa862ad225c1afe85fbc16ff17
sungkuk5420 <sungkuk5420@gmail.com>
2021-08-05 00:00:23 +0900

    채팅목록 디자인대로 수정

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -4,12 +4,16 @@
       <img :src="chat?chat.avatar:''" alt />
     </div>
     <div class="chatting-text">
-      <div class="chat-name">
-        <b>{{chat?chat.name:''}}</b>
-        <div class="chatting-date">{{chat.time}}</div>
+      <div class="chatting-left">
+        <div class="chat-name">
+          <b>{{chat?chat.name:''}}</b>
+        </div>
+        <div class="chat-conversation">
+          {{chat.text}}
+        </div>
       </div>
-      <div class="chat-conversation">
-        <div class>{{chat.text}}</div>
+      <div class="chatting-right">
+        <img src="statics/icons/btn_chat_start.jpg" alt  style="width:55px;"/>
       </div>
     </div>
   </div>
@@ -25,36 +29,22 @@ export default {
 <style lang="scss">
 .chat {
   display: flex;
-  padding: 10px;
+  padding: 5px 10px;
   border-bottom: 1px solid #d8d9d9;
-  min-height: 50px;
   cursor: pointer;
 }
 .chat .chat-img {
   width: 60px;
+  display: flex;
+  flex:0;
+  align-items: center;
   img {
     width: 60px;
     height: 60px;
     border-radius: 50%;
   }
 }
-.chat .chatting-text {
-  justify-content: flex-start;
-  flex: 1;
-  padding-left: 20px;
-  overflow: hidden;
-}
-.chat .chatting-text .chat-name {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  b {
-    color: #2a9898;
-    font-size: 16px;
-  }
-}
-
-.chat .chatting-text .chat-conversation {
+.chat .chat-conversation {
   padding-top: 10px;
   font-size: 14px;
   display: flex;
@@ -64,8 +54,29 @@ export default {
     white-space: nowrap;
   }
 }
-.chat .chatting-date {
-  width: 110px;
-  font-size: 12px;
+.chatting-text{
+  justify-content: flex-start;
+  flex: 1;
+  padding-left: 20px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .chat-name {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    b {
+      color: #2a9898;
+      font-size: 16px;
+    }
+  }
+  .chatting-left{
+
+  }
+  .chatting-right{
+    padding-top: 10px;
+    
+  }
 }
 </style>
